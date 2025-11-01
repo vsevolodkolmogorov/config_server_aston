c44926b1fee6c668ad145ec7e562514ed6ffd5da
vilar <loadslowly@gmail.com>
2025-11-01 21:26:25 +0500

    init: config server for all my services

---
+++ src/test/java/ru/astondevs/configserveraston/ConfigServerAstonApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.astondevs.configserveraston;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerAstonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
